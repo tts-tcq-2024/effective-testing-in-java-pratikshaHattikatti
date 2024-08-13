efd3ce25381700f8c07943a0854c06ebfda4881a
pratikshaHattikatti <52101504+pratikshaHattikatti@users.noreply.github.com>
2024-08-13 15:21:07 +0530

    Update tshirts.java

--- tshirts/tshirts.java
+++ tshirts/tshirts.java
@@ -13,7 +13,7 @@ public class Tshirts {
         assert(size(38) == "S");
         assert(size(50) == "L");
         assert(size(40) == "M");
-        assert(size(43) == "L");
+        assert(size(42) == "L");
         System.out.println("All is well (maybe!)");
     }
 }
