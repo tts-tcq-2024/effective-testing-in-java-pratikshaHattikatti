425659492e2db0388572748d33a078cfb20c8c6a
pratikshaHattikatti <52101504+pratikshaHattikatti@users.noreply.github.com>
2024-08-27 14:51:56 +0530

    Update misaligned.java

--- misaligned/misaligned.java
+++ misaligned/misaligned.java
@@ -1,17 +1,30 @@
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 public class Misaligned {
     static int printColorMap() {
         String majorColors[] = {"White", "Red", "Black", "Yellow", "Violet"};
         String minorColors[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+        StringBuilder sb = new StringBuilder();
         int i = 0, j = 0;
         for(i = 0; i < 5; i++) {
             for(j = 0; j < 5; j++) {
-                System.out.printf("%d | %s | %s\n", i * 5 + j, majorColors[i], minorColors[i]);
+                sb.append(String.format("%d | %s | %s\n", i * 5 + j, majorColors[i], minorColors[j])); // Fixed: minorColors[j] to correctly align
             }
         }
+        System.out.println(sb.toString());
         return i * j;
     }
-    public static void main(String[] args) { 
+
+    public static void main(String[] args) {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        PrintStream originalOut = System.out;
+        System.setOut(new PrintStream(baos));
         int result = printColorMap();
+        System.setOut(originalOut);
+        String output = baos.toString();
+        assert(output.contains("0 | White | Blue"));
+        assert(output.contains("4 | White | Slate"));
         assert(result == 30);
         System.out.println("All is well (maybe!)");
     }
